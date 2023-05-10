545decd9af1eccd17d72f11665caab37130b9bec
alejandro <alejandrozapata@gmail.com>
2023-05-09 21:12:59 -0300

    ultimo funcion

---
+++ clase5/script.js
@@ -0,0 +1,198 @@
+
+
+// // console.log("hola funciones")
+
+// // // and... or.. not
+
+// // //...........f.......t
+// // console.log((4>5)&&(3>2))//
+
+// // if((tarjeCredito==true) && (saldoFavor==true)){
+
+// //     alert("puedo utilizar el hotsale")
+
+// // }
+
+
+// // if((robot==true) || (humano==true) ){
+
+// //     alert("puede hacer una compra")
+
+// // }
+
+
+// // if(telefono=true || email==true){
+
+// // // }
+
+// // // if(telefonoEmail==true && pass==true && recuerdame==true){
+
+// // // }
+
+// // // //... (front html ...js)....backend
+
+
+// // // var sumaA = 25
+// // // var sumaB = 30
+
+// // // var sumaNumeros = sumaA + sumaB
+
+
+// // // var sumarNumerosX = sumaB + SumaC
+
+
+// // // function saludar(){
+
+// // //     alert("hola Mundo Funciones")
+// // //     console.log("hola mundo por consola")
+// // // }
+
+
+// // // saludar()
+
+// // // saludar()
+
+// // // saludar()
+
+// // // saludar()
+
+// // // console.log("hola mundo funciones")
+// // // console.log("hola mundo funciones")
+// // // console.log("hola mundo funciones")
+// // // console.log("hola mundo funciones")
+
+
+// // //ejemplo slide
+// // // function solicitarNombre(){
+// // //     let nombreIngresado   = prompt("Ingresar nombre")
+// // //     alert("El nombre ingresado es " + nombreIngresado)
+// // // } 
+
+
+// // // solicitarNombre()
+
+// // var numeroX = 20
+// // let sumar=120
+
+// // function sumarNumerosA(numero1,numero2) {
+// //     //funcionan en el ambito de la funcion
+// //     sumar = numero1 + numero2
+
+// //     alert("el resultado de la suma es: " + sumar)//80
+
+// //     console.log("imprimiendo variable global" + numeroX)//20
+
+// // }
+
+// // let numero1=30
+// // let numero2=50
+
+// // console.log(sumar)//
+// // sumarNumerosA(numero1,numero2)
+// // console.log(sumar)//
+
+// // console.log("llamando a varible local de funcion: " + sumar)//
+
+
+// // function devolverAlgo(){
+
+// //     console.log("hola")
+
+// //     return "hola"
+// // }
+
+
+// // function sumar(primerNumero, segundoNumero) {
+    
+    
+// //     return primerNumero + segundoNumero;
+
+// // }
+
+
+// // // sumar(5,8)//
+
+// // console.log(devolverAlgo())//
+
+// // let resultado = sumar(5, 8);
+
+// function mayorEdad(edad){
+
+//     if(edad>=18){
+
+       
+//         return true
+
+//     } else {
+
+//         return false
+
+//     }
+
+    
+// }
+
+
+// let resultadoEvaluacion = mayorEdad(14)
+
+// // function probando(resultadoEvaluacion){
+
+
+// // }
+
+// console.log(resultadoEvaluacion)
+
+// if(resultadoEvaluacion==false){
+//     alert("es menor")
+// }
+
+
+
+
+
+
+// const suma  = function (a, b) 
+// {
+//      return a + b 
+// }
+
+
+// console.log( suma(15,20) )
+
+// const suma  = (a, b) => {
+//      return a + b 
+//     }
+
+// console.log( suma(15,20) )
+
+
+var nombreUsuario = prompt("ingrese su nombre")
+
+if(nombreUsuario==X){
+
+}
+
+
+for(let i=0;i<3;i++){
+
+    prompt("ingrese cantidad de clientes")
+
+}
+
+
+var precio = 15
+
+var cantidad = 3
+
+
+function totalPagar(precio,cantidad){
+
+    alert(precio * cantidad)
+
+}
+
+function totalPagar(precio,cantidad){
+
+    return precio * cantidad
+
+}
